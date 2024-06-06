cba14e56e797fb74bece6891aa40f7d16a48cb58
lumingxu2020 <65892612+lumingxu2020@users.noreply.github.com>
2024-06-06 10:12:11 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -104,10 +104,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '在线绘画',
+  HEO_SOCIAL_CARD_TITLE_2: '发挥你的想象力',
+  HEO_SOCIAL_CARD_TITLE_3: '点击进入绘图空间',
+  HEO_SOCIAL_CARD_URL: 'https://mj.moming.chat',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
