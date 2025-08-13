8971ef0a2fc0eab202cf15416c0a0ca04f9d43f9
lumingxu2020 <65892612+lumingxu2020@users.noreply.github.com>
2025-08-13 15:22:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -35,7 +35,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://pica.zhimg.com/80/v2-22cfc697da4a32de29bfeb0d95b3c65e_1440w.webp?spaceId=44df280e-dbc7-42ed-b9ef-cc21610290ee&t=1d8159b5-7644-800b-81ad-d4a1c0c1a81a', // 英雄区右侧图片
+  //   HERO_RECOMMEND_COVER: 'https://pica.zhimg.com/v2-22cfc697da4a32de29bfeb0d95b3c65e_1440w.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
